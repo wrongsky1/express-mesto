ac6d540c604bea5e3ac6ddeff2d69f0cd58fd908
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-09-20 13:47:43 +0300

    1. Подключитесь к Mongo

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 
-const path = require('path');
+// const path = require('path');
 const usersRouters = require('./routes/users.js');
 const cardsRouters = require('./routes/cards.js');
 
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const { PORT = 3000 } = process.env;
+
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use('/', usersRouters);
 app.use('/', cardsRouters);
