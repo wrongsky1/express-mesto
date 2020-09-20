69c832333ad43b716bd7d14bb23d868ed1c680ef
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-09-20 17:48:29 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const path = require('path');
+// const path = require('path'); // el
+
 const usersRouters = require('./routes/users.js');
 const cardsRouters = require('./routes/cards.js');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 5000 } = process.env;
 
 const app = express();
+// app.use(express.static(path.join(__dirname, 'public'))); // el
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+/*
+app.use((req, res, next) => {
+  req.user = {
+    id: '5d8b8592978f8bd833ca8133', // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
 
+  next();
+});
+/*
+{
+    "name": "тестовый пользователь",
+    "about": "информация о себе",
+    "avatar": "https://memepedia.ru/wp-content/uploads/2020/06/elli-mem-the-last-of-us-2.jpg"
+}
+*/
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
--- controllers/cards.js
+++ controllers/cards.js
@@ -81,3 +81,20 @@ module.exports = {
   likeCard,
   deleteLikeCard,
 };
+
+/*
+const path = require('path');
+const readFile = require('../helpers/readFile.js');
+
+const getCards = (req, res) => readFile(path.join(__dirname, '..', 'data', 'cards.json'))
+  .then((data) => {
+    res.status(200).send(JSON.parse(data));
+  })
+  .catch((err) => {
+    res.status(500).send({ message: err.message });
+  });
+
+module.exports = {
+  getCards,
+};
+*/
